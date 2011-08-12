fc3f09b3409f8a85e5fb44ec12795fe4f296b587
Taliesin Sisson <taliesins@yahoo.com>
2011-08-12 21:32:40 +0100

    Add missing cache files

---
+++ Talifun.Web/Cache/HttpCacheManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+
+namespace Talifun.Web
+{
+    public class HttpCacheManager : ICacheManager
+    {
+        protected readonly System.Web.Caching.Cache Cache = HttpRuntime.Cache;
+
+        public T Get<T>(string key) where T: class
+        {
+            return Cache.Get(key) as T;
+        }
+
+        public void Insert<T>(string key, T value, System.Web.Caching.CacheDependency dependencies, System.DateTime absoluteExpiration, System.TimeSpan slidingExpiration, System.Web.Caching.CacheItemPriority priority, System.Web.Caching.CacheItemRemovedCallback onRemoveCallback) where T : class
+        {
+            Cache.Insert(key, value, dependencies, absoluteExpiration, slidingExpiration, priority, onRemoveCallback);
+        }
+
+        public T Remove<T>(string key) where T : class
+        {
+            return Cache.Remove(key) as T;
+        }
+    }
+}
---
+++ Talifun.Web/Cache/ICacheManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Caching;
+
+namespace Talifun.Web
+{
+    public interface ICacheManager
+    {
+        T Get<T>(string key) where T : class;
+
+        void Insert<T>(
+            string key,
+            T value,
+            CacheDependency dependencies,
+            DateTime absoluteExpiration,
+            TimeSpan slidingExpiration,
+            CacheItemPriority priority,
+            CacheItemRemovedCallback onRemoveCallback) where T : class;
+        T Remove<T>(string key) where T : class;
+    }
+}
