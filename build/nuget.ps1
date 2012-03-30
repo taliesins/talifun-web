e8b850f19b4fd470f0f3a531114933d96535f75c
Taliesin Sisson <taliesins@yahoo.com>
2012-03-30 07:01:20 +0100

    Support building multiple nuget packages Add mvc 4 as a package Point mvc examples to mvc 4 package

--- build/nuget.ps1
+++ build/nuget.ps1
@@ -4,7 +4,6 @@
 # Dual-licensed under the Apache License, Version 2.0, and the Microsoft Public License (Ms-PL).
 # See the file LICENSE.txt for details.
 # 
-
 properties {
   Write-Output "Loading nuget properties"
   # see if we should be using chewie, load if needed
@@ -14,21 +13,19 @@ properties {
   $nuget = @{}
   $nuget.pub_dir = "$($release.dir)"
   $nuget.file = (Resolve-NuGet)
-  Write-Output $nuget.file
+  
   # add either the project_name or nuspec file to use when packaging.
-  $nuget.target = "$($base.dir)\$($solution.name).nuspec"
   $nuget.options = ""
-  if(!(Test-Path($nuget.target))) { 
-    Write-Output "Could not find $($nuget.target)" 
-    $nuget.target = "$($source.dir)\$($solution.name).nuspec"
-  }
-  if(!(Test-Path($nuget.target))) {  
-    Write-Output "Could not find $($nuget.target)" 
-    $nuget.target = "$($source.dir)\$($solution.name)\$($solution.name).csproj"
-    $nuget.options = "-Build -Sym -Properties Configuration=$($build.configuration)"
-  }
-  if(!(Test-Path($nuget.target))) {  
-    Write-Output "Could not find $($nuget.target)" 
+  $nuget.targets = @((Get-ChildItem -path "$($base.dir)\*" -recurse -include *.nuspec) | Select $_.FullName)
+
+  if ($nuget.targets.length -lt 1 ){
+    if(!(Test-Path("$($source.dir)\$($solution.name)\$($solution.name).csproj"))) {  
+		Write-Output "Could not find $($source.dir)\$($solution.name)\$($solution.name).csproj" 
+		$nuget.targets = @()
+	} else {
+		$nuget.targets = @("$($source.dir)\$($solution.name)\$($solution.name).csproj")
+		$nuget.options = "-Build -Sym -Properties Configuration=$($build.configuration)"
+	}
   }
 }
 
@@ -50,18 +47,21 @@ Task Create-NuGetPackage -depends Set-NuSpecVersion {
   Assert (![string]::IsNullOrEmpty($nuget.file) -and (Test-Path($nuget.file))) "The location of the nuget exe must be specified."
   Assert (Test-Path($nuget.file)) "Could not find nuget exe"
 
-  $nuget.command = "& $($nuget.file) pack $($nuget.target) $($nuget.options) -Version $($build.version) -OutputDirectory $($nuget.pub_dir)"
+  foreach ($nuget_target in $nuget.targets)
+  {
+	  $nuget.command = "& $($nuget.file) pack $nuget_target $($nuget.options) -Version $($build.version) -OutputDirectory $($nuget.pub_dir)"
   
-  if(!(Test-Path($nuget.pub_dir))) { new-item $nuget.pub_dir -itemType directory | Out-Null }
-  $nugetTargetPath = (Split-Path $nuget.target)
-  Write-Output "Moving into $nugetTargetPath"
-  Push-Location $nugetTargetPath
-  try {
-    $message = "Error executing command: {0}"
-    $command = "Invoke-Expression $($nuget.command)"
-    $errorMessage = $message -f $command
-    exec { Invoke-Expression $nuget.command } $errorMessage
-  } finally { Pop-Location }
+	  if(!(Test-Path($nuget.pub_dir))) { new-item $nuget.pub_dir -itemType directory | Out-Null }
+	  $nuget_targetPath = (Split-Path $nuget_target)
+	  Write-Output "Moving into $nuget_targetPath"
+	  Push-Location $nuget_targetPath
+	  try {
+		$message = "Error executing command: {0}"
+		$command = "Invoke-Expression $($nuget.command)"
+		$errorMessage = $message -f $command
+		exec { Invoke-Expression $nuget.command } $errorMessage
+	  } finally { Pop-Location }
+  }
 }
 
 Task Publish-NuGetPackage {
@@ -72,9 +72,13 @@ Task Publish-NuGetPackage {
 }
 
 Task Set-NuSpecVersion {
-  Assert (![string]::IsNullOrEmpty($nuget.target) -and (Test-Path($nuget.target))) "The location of the nuspec file must be specified."
+  Assert ($nuget.targets.length -gt 0) "The location of the nuspec file must be specified."
 
   $version_pattern = "<version>\d*\.\d*\.\d*.\d*</version>"   # 3 digit for semver
-  $content = Get-Content $nuget.target | % { [Regex]::Replace($_, $version_pattern, "<version>$($build.version)</version>") } 
-  Set-Content -Value $content -Path $nuget.target
+
+  foreach ($nuget_target in $nuget.targets)
+  {
+	$content = Get-Content "$nuget_target" | % { [Regex]::Replace($_, $version_pattern, "<version>$($build.version)</version>") } 
+	Set-Content -Value $content -Path $nuget_target
+  }
 }
