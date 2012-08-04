8445ec92a46676925e579a4b5d66d0fee8e4699e
Taliesin Sisson <taliesins@yahoo.com>
2012-08-04 12:44:49 +0100

    Build works for 3.5 & 4.0

--- build/msbuild.ps1
+++ build/msbuild.ps1
@@ -17,8 +17,9 @@ properties {
 }
 
 Task Invoke-MsBuild {
-  $command = "msbuild /m:$($msbuild.max_cpu_count) /p:BuildInParralel=$($msbuild.build_in_parralel) `"/logger:$($msbuild.logger);logfile=$($msbuild.logfilepath)\$($msbuild.logfilename)`" /p:Configuration=`"$($build.configuration)`" /p:Platform=`"$($msbuild.platform)`" /p:OutDir=`"$($build.dir)`"\\ `"$($solution.file)`""
+  $TargetFrameworkVersion = "v$framework"
+  $command = "msbuild /m:$($msbuild.max_cpu_count) /p:BuildInParralel=$($msbuild.build_in_parralel) `"/logger:$($msbuild.logger);logfile=$($msbuild.logfilepath)\$($msbuild.logfilename)`" /p:Configuration=`"$($build.configuration)`" /p:TargetFrameworkVersion=`"$($TargetFrameworkVersion)`" /p:Platform=`"$($msbuild.platform)`" /p:OutDir=`"$($build.dir)`"\\ `"$($solution.file)`""
   $message = "Error executing command: {0}"
   $errorMessage = $message -f $command
-  exec { msbuild /m:"$($msbuild.max_cpu_count)" "/p:BuildInParralel=$($msbuild.build_in_parralel)" "/logger:$($msbuild.logger);logfile=$($msbuild.logfilepath)\$($msbuild.logfilename)" /p:Configuration="$($build.configuration)" /p:Platform="$($msbuild.platform)" /p:OutDir="$($build.dir)"\\ "$($solution.file)" } $errorMessage
+  exec { msbuild /m:"$($msbuild.max_cpu_count)" "/p:BuildInParralel=$($msbuild.build_in_parralel)" "/logger:$($msbuild.logger);logfile=$($msbuild.logfilepath)\$($msbuild.logfilename)" /p:Configuration="$($build.configuration)" /p:TargetFrameworkVersion="$($TargetFrameworkVersion)" /p:Platform="$($msbuild.platform)" /p:OutDir="$($build.dir)"\\ "$($solution.file)" } $errorMessage
 }
